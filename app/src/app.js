5ef15bde2e0f7c0d806d145d2b34ae73bc766e9f
pratik3098 <pratik3098@gmail.com>
2020-02-14 18:57:40 -0500

    Connecttion added using prostges.js

--- app/src/app.js
+++ app/src/app.js
@@ -3,14 +3,17 @@ const xlsx = require('xlsx')
 const path =require('path')
 
 const config= {
-    database: 'fin-rec',
+    database: 'fin-rec-db',
     user: 'postgres',
     password: 'somepass',
     port: 5432,
-    host: 'localhost'
+    host: 'localhost',
+    connection: {
+        application_name: 'facedrive-db-connection'
+    }
 }
-const client = postgres(config)
-console.log(client)
+const sql = postgres('postgres://'+config.user+':'+config.password+'@'+config.host+':'+config.port+'/'+config.database, config)
+console.log(sql)
 
 let fc_file= xlsx.readFile(path.resolve(__dirname,"./fc_data.xlsx"))
 let stripe_file = xlsx.readFile(path.resolve(__dirname,'./stripe_data.xlsx'))
@@ -21,3 +24,41 @@ let stripe_data = xlsx.utils.sheet_to_json(stripe_file.Sheets[stripe_file.SheetN
 
 //console.log(fc_data)
 //console.log(stripe_data)
+
+async function createTable(){
+    await sql`
+    CREATE TABLE facedrive (
+        ride_id                   VARCHAR(35) PRIMARY KEY,
+        ride_created              TIMESTAMP NOT NULL,
+        ride_status               VARCHAR(20) CHECK (ride_status IN (NOT_ACCEPTED, FAILED, FINISHED, CANCELLED_BY_DRIVER, CANCELLED_REQUEST ) ) NOT NULL,
+        ride_region               VARCHAR(15) NOT NULL,
+        rp_client_pay             INTEGER,
+        rp_facedrive_fee          INTEGER,
+        rp_ride_status            VARCHAR(6) CHECK (rp_ride_status IN (PAID, FAILED)),
+        rp_toll_roads             VARCHAR(20),
+        rp_carbon_offset          INTEGER, 
+        rp_driver_earnings        INTEGER,
+        rp_driver_tax             INTEGER,
+        rp_client_tax             INTEGER,
+        rp_base_fare              INTEGER,
+        rp_facedrive_fee          INTEGER,
+        up_client_pay             INTEGER,
+        up_facedrive_fee          INTEGER,
+        up_tips                   INTEGER,
+        up_payment_status         VARCHAR(6) CHECK (up_payment_staus IN (PAID, FAILED)),
+        stripe_reserve_charge_id  VARCHAR(27) UNIQUE,
+        amount_charged_id         VARCHAR(27) UNIQUE,
+        up_amount_charged         INTEGER,
+        coupon_name               VARCHAR(20),
+        coupoun_dollar_off        INTEGER,
+        coupon_percent_off        INTEGER,
+        coupon_applied_status     VARCHAR(9) CHECK (coupon_applied_status IN (succeeded)),
+        coupon_amount_charged     INTEGER
+    );
+   `
+}
+
+/*createTable().then(res=>{
+    console.log(res)
+}).catch(err=>{console.error(err)})
+*/

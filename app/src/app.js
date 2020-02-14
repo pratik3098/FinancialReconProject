1deb53b65bf0006ef5d7663e2a7743dbd9ed863b
pratik3098 <pratik3098@gmail.com>
2020-02-14 17:57:04 -0500

    Excel inpu added

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,6 @@
 const postgres = require('postgres')
+const xlsx = require('xlsx')
+const path =require('path')
 
 const config= {
     database: 'fin-rec',
@@ -7,9 +9,15 @@ const config= {
     port: 5432,
     host: 'localhost'
 }
-
 const client = postgres(config)
-
 console.log(client)
 
+let fc_file= xlsx.readFile(path.resolve(__dirname,"./fc_data.xlsx"))
+let stripe_file = xlsx.readFile(path.resolve(__dirname,'./stripe_data.xlsx'))
 
+// JSON Arrays of the sheet data
+let fc_data = xlsx.utils.sheet_to_json(fc_file.Sheets[fc_file.SheetNames[0]])
+let stripe_data = xlsx.utils.sheet_to_json(stripe_file.Sheets[stripe_file.SheetNames[0]])
+
+//console.log(fc_data)
+//console.log(stripe_data)
